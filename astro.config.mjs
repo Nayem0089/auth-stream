a70d4000ae2bfbdb819e1d2c619bb24d745ac5fe
Nayem0089 <mmnadimqwq8833@gmail.com>
2026-08-28 15:16:34 +0600

    Delete astro.config.mjs

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-
-export default defineConfig({
-  site: 'https://streamauth.dev',
-  integrations: [sitemap()],
-  output: 'static',
-  compressHTML: true,
-});
